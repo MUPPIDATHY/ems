4fab8e43ddf943cdcd175b5cb45da0d32ad21e54
ASHIANTEC <81386686+ASHIANTEC@users.noreply.github.com>
2021-08-04 14:11:29 +0530

    dump

---
+++ Dump20210804/ems_cand_address_details.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ems
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cand_address_details`
+--
+
+DROP TABLE IF EXISTS `cand_address_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cand_address_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cand_id` varchar(900) DEFAULT NULL,
+  `address_type` varchar(900) DEFAULT NULL,
+  `ps_address` varchar(900) DEFAULT NULL,
+  `ps_pincode` varchar(900) DEFAULT NULL,
+  `ps_state` varchar(900) DEFAULT NULL,
+  `ps_district` varchar(900) DEFAULT NULL,
+  `pm_address` varchar(900) DEFAULT NULL,
+  `pm_pincode` varchar(900) DEFAULT NULL,
+  `pm_state` varchar(900) DEFAULT NULL,
+  `pm_district` varchar(900) DEFAULT NULL,
+  `last_modified_time` varchar(900) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=371 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cand_address_details`
+--
+
+LOCK TABLES `cand_address_details` WRITE;
+/*!40000 ALTER TABLE `cand_address_details` DISABLE KEYS */;
+INSERT INTO `cand_address_details` VALUES (368,'devi344125','1','120 b kottur road palayamkottai','627002','Tamil Nadu','tirunelveli','120 b kottur road palayamkottai','627002','Tamil Nadu','tirunelveli','08/03/2021 16:16:17'),(369,'ravi969702','1','','',' ','','','',' ','','08/03/2021 12:35:43'),(370,'maha242376','1','','',' ','','','',' ','','08/03/2021 15:46:51');
+/*!40000 ALTER TABLE `cand_address_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-04 14:09:18
